4e4b8bfb7d3ec0cefa8afcb6ed89406bc6c0a9e0
Ollie Read <code@ollieread.com>
2026-03-29 20:48:14 +0100

    chore(database): Create connection factory

---
+++ src/Database/ConnectionFactory.php
@@ -0,0 +1,149 @@
+<?php
+declare(strict_types=1);
+
+namespace Engine\Database;
+
+use Engine\Database\Config\ConnectionConfig;
+use Engine\Database\Config\DatabaseConfig;
+use Engine\Database\Exceptions\ConnectionException;
+use Engine\Database\Exceptions\DatabaseException;
+use PDO;
+use PDOException;
+
+/**
+ * Connection factory
+ * ------------------
+ *
+ * Responsible for creating database connections and storing them in a cache.
+ */
+final class ConnectionFactory
+{
+    /**
+     * Default PDO options.
+     *
+     * @var array<int, int|bool>
+     */
+    private static array $defaultOptions = [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false,
+        PDO::ATTR_STRINGIFY_FETCHES  => false,
+    ];
+
+    /**
+     * Cached database connections.
+     *
+     * @var array<string, Connection>
+     */
+    private array $connections = [];
+
+    /**
+     * @param DatabaseConfig $config
+     */
+    public function __construct(
+        private readonly DatabaseConfig $config,
+    ) {
+    }
+
+    /**
+     * Get a database connection.
+     *
+     * Retrieves a cached connection if it exists, otherwise creates a new one.
+     * If no name is provided, the primary connection is returned.
+     *
+     * @param string|null $name
+     *
+     * @return Connection
+     */
+    public function make(?string $name = null): Connection
+    {
+        $name ??= $this->config->primary;
+
+        return $this->connections[$name] ?? ($this->connections[$name] = $this->createConnection($name));
+    }
+
+    /**
+     * Create a new database connection.
+     *
+     * @param string $name
+     *
+     * @return Connection
+     */
+    private function createConnection(string $name): Connection
+    {
+        // Grab the config.
+        $config = $this->config->connections[$name] ?? null;
+
+        // In theory, this should never happen, but it's here just in case.
+        if ($config === null) {
+            throw ConnectionException::noConfig($name);
+        }
+
+        try {
+            // Create the connection.
+            return new Connection(
+                $name,
+                $this->createPdo($config),
+            );
+        } catch (PDOException $e) {
+            // If there was a PDO problem, throw an exception.
+            throw ConnectionException::cannotConnect($name, $e);
+        }
+    }
+
+    /**
+     * Create a PDO instance.
+     *
+     * @param ConnectionConfig $config
+     *
+     * @return PDO
+     */
+    private function createPdo(ConnectionConfig $config): PDO
+    {
+        $options = array_merge(
+            self::$defaultOptions,
+            $config->options,
+        );
+
+        // This should only ever create a MySQL instance because the driver is
+        // hardcoded, but this is here for the future.
+        return new PDO(match ($config->driver) {
+            'mysql' => $this->createMysqlPdoDsn($config),
+            default => throw new DatabaseException(
+                sprintf(
+                    'Unsupported database driver: %s.',
+                    $config->driver,
+                ),
+            ),
+        }, $config->username, $config->password, $options);
+    }
+
+    /**
+     * Create a PDO DSN string for MySQL.
+     *
+     * @param ConnectionConfig $config
+     *
+     * @return string
+     */
+    private function createMysqlPdoDsn(ConnectionConfig $config): string
+    {
+        if ($config->socket) {
+            return sprintf(
+                'mysql:unix_socket=%s;dbname=%s',
+                $config->socket,
+                $config->database,
+            );
+        }
+
+        if ($config->host) {
+            return sprintf(
+                'mysql:host=%s;port=%d;dbname=%s',
+                $config->host,
+                $config->port ?? 3307,
+                $config->database,
+            );
+        }
+
+        throw new DatabaseException('No host or socket specified.');
+    }
+}
